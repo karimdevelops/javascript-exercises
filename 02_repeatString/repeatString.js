138a5c1bbb8fef63ab163479b81e7344c3306859
Abdul Karim <karim.develops@gmail.com>
2024-09-04 12:18:45 +0500

    Error handling for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+
   let newString = "";
   for (let i = 1; i <= num; i++) {
     newString = newString.concat(string);
