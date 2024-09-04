29e533381ad775a22f79e7ea0851d033b0445903
Abdul Karim <karim.develops@gmail.com>
2024-09-04 20:30:47 +0500

    Sum all int between a given range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (startInt, stopInt) {
+  let total = 0;
+  for (let i = startInt; i <= stopInt; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
