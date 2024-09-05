31c82a648de69f8f3b2685edf0c92b0005fb5de8
Abdul Karim <karim.develops@gmail.com>
2024-09-05 05:52:13 +0500

    Identify larger numbers first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function (startInt, stopInt) {
+  let start = startInt < stopInt ? startInt : stopInt;
+  let stop = stopInt > startInt ? stopInt : startInt;
   let total = 0;
-  for (let i = startInt; i <= stopInt; i++) {
+  for (let i = start; i <= stop; i++) {
     total += i;
   }
   return total;
