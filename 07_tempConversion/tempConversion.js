903aa03bb8e8d2976c445e9bf3c91881994a6c37
Abdul Karim <karim.develops@gmail.com>
2024-09-06 08:24:12 +0500

    Round temps to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const convertToCelsius = function (temp) {
   let tempCelcius = (temp - 32) * (5 / 9);
+  tempCelcius = Math.round(tempCelcius * 10) / 10;
   return tempCelcius;
 };
 
 const convertToFahrenheit = function (temp) {
   let tempFahrenheit = temp * (9 / 5) + 32;
+  tempFahrenheit = Math.round(tempFahrenheit * 10) / 10;
   return tempFahrenheit;
 };
 // Do not edit below this line
