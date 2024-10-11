9ac07fb3e78c3eed7d622f74c234d7b671974525
Abdul Karim <karim.develops@gmail.com>
2024-10-11 21:11:12 +0500

    Create a function to check for a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let arr = [...str].filter(
+    (x) =>
+      x != " " &&
+      x != null &&
+      ((x >= "a" && x <= "z") || (x >= "A" && x <= "Z") || (x >= 0 && x <= 9))
+  );
+  let alphaStr = arr.join("").toLowerCase();
+  let alphaReversedStr = arr.reverse().join("").toLowerCase();
+  return alphaStr == alphaReversedStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
