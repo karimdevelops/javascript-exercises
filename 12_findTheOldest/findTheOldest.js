0106a2d5ccb858c76228eff1f46f452dd6b74064
Abdul Karim <karim.develops@gmail.com>
2024-10-12 21:00:05 +0500

    Create function to return oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  oldstToYngst = arr.sort((a, b) => {
+    return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+  });
+  oldest = oldstToYngst[0];
+  return oldest;
 };
 
 // Do not edit below this line
